7291ac6164a52c8d06c5639ce35554504fcc971d
danJecu <jdanalexandru@gmail.com>
2023-12-21 10:32:45 +0100

    add knex config

---
+++ api/src/database/knexfile.ts
@@ -0,0 +1,26 @@
+import type { Knex } from 'knex';
+
+interface IKnexConfig {
+  [key: string]: Knex.Config;
+}
+
+const configs: IKnexConfig = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      port: 2022,
+      user: 'postgres',
+      password: 'postgres',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+};
+
+export default configs;
